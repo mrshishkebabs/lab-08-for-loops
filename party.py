aeee17343fe0a77d895b1013bee562b7668e6408
Ken Park <xkenxparkx@gmail.com>
2021-11-01 15:56:04 -0400

    Party Time! For Py and Html

---
+++ party.py
@@ -0,0 +1,8 @@
+﻿userinput = input("Let's party! How long till the party? ");
+usernum = int(userinput, 10);
+if usernum < 1: 
+	print("PARTY NOW!!!!!");     
+else:
+	for i in range(usernum, 0, -1):
+		print(i);
+	print("PARTY TIME!!!!!");
