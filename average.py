d4a25eb757bbdd58ec9ba230f6076959bb6bb9bc
Ken Park <xkenxparkx@gmail.com>
2021-11-01 16:14:23 -0400

    Python of an Average file

---
+++ average.py
@@ -0,0 +1,11 @@
+﻿average = 0
+sum = 0
+
+for i in range(0, 4):
+    userinput = input("Number please... ")
+    usernum = int(userinput, 10)
+    sum = sum + usernum
+    print("User input is: " + str(usernum) + " and the current sum is: " + str(sum));
+
+average = sum / 4;
+print("The average is: " + str(average));
