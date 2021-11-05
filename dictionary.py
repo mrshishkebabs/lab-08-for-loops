068c9c7fddba07dd57b91cec1a9156c0bcc82669
Ken Park <xkenxparkx@gmail.com>
2021-11-05 14:49:22 -0400

    Dictionary for dummies...or python only

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+﻿myData = {"effective top tube length": 515, "seat tube length": 500, "seat tube angle": 74.4, "head tube angle": 70.5, "stack": 513, "reach": 367, "standover height": 755}
+
+keys = []
+values = []
+
+for k, v in myData.items():
+    keys.append(k)
+    values.append(v)
+    print("key: ", k, ", value: ", v)
+print("ALL KEYS: ", keys)
+print("ALL VALUES: ", values)
